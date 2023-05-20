39fa5b5856d1926585d6b818bfb75b971a620a6d
Prakhar Sachan <91385023+PrakharSachan5342@users.noreply.github.com>
2023-05-20 12:51:08 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,13 @@
+#its our personal api key , anyone can use their api key and can run this bot . Make sure to upload this file to the enviroment.
+
+API_KEY = "PK4U74KU96REG98GM9KS"
+SECRET_KEY = "N6DG4ZRUSIl1E7fUgdYdMydWQJC41tat33eVQHuB"
+
+ALPACA_CONFIG = {
+# Put your own Alpaca key here:
+"API_KEY": "PK4U74KU96REG98GM9KS",
+# Put your own Alpaca secret here:
+"API_SECRET": "N6DG4ZRUSIl1E7fUgdYdMydWQJC41tat33eVQHuB",
+# If you want to go live, you must change this
+"ENDPOINT": "https://paper-api.alpaca.markets",
+}
